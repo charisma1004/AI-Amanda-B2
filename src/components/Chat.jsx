fce8409472706da547456274f8a9581d9f340f8d
charisma1004 <taman.contact.hello@gmail.com>
2025-04-29 13:34:17 +0900

    after fixing

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Chat = () => {
     return (
-        <div style={{ position: 'fixed', bottom: '20px', right: '20px', width: '400px', height: '500px', zIndex: '1000' }}>
+        <div style={{ position: 'absolute', bottom: '0px', right: '20px', width: '400px', height: '500px', zIndex: '1000' }}>
             <iframe
                 src="https://www.chatbase.co/chatbot-iframe/cgwZY9BIvUQWivQsPUECX"
                 title="Chatbot"
